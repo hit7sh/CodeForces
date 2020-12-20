81f1e0ffe21c956f4a2153c527950fa5fac0f822
Hitesh Kumar Saini <hit7sh@gmail.com>
2020-12-20 15:11:36 +0530

    Create B.cpp

---
+++ Round 691 div 2/B.cpp
@@ -0,0 +1,40 @@
+// Author :: <Hitesh_Saini>
+#include<bits/stdc++.h>
+
+#define __speed() ios_base::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+#define dbg(x) cout << "--- " << #x << " = " << x << " ---\n";
+#define findmax(v) *max_element(v.begin(), v.end())
+#define _sum(a) accumulate(a.begin(), a.end(), 0)
+#define tt int t;for(cin>>t; t--; )
+#define f0(i, n) for(i = 0; i < int(n); i++)
+#define f1(i, n) for(i = 1; i<= int(n); i++)
+#define all(x) x.begin(), x.end()
+#define EB emplace_back
+#define PB push_back
+#define F first
+#define S second
+#define endl "\n"
+
+
+using namespace std;
+
+template<typename... Args>void print(Args... args){((cout << args << " "), ...);cout << endl;}
+
+void solve(){//c
+	int i, n;
+	cin >> n;
+	if(n&1)
+		print(2*(n/2+1)*(n/2+2));
+	else
+		print((n/2+1)*(n/2+1));
+}
+
+int main(){
+	#ifndef ONLINE_JUDGE
+		freopen("input.txt", "r", stdin);
+		freopen("output.txt", "w", stdout);
+	#endif
+    __speed();
+    solve();
+    return 0;
+}
