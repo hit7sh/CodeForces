5415a4c75ee99a4ec250842335c560f9730b0b4c
Hitesh Kumar Saini <hit7sh@gmail.com>
2021-01-26 12:22:53 +0530

    Create A.cpp

---
+++ Round 697 div 3/A.cpp
@@ -0,0 +1,41 @@
+// Author :: <Hitesh_Saini>
+#include<bits/stdc++.h>
+
+#define Yes(x) print((x) ? "Yes" : "No")
+#define tt 	int t; for (cin >> t; t--; )
+
+
+
+using namespace std;
+using ll = long long;
+
+
+
+template <typename... T> void print(T... args) { ((cout << args << " "), ...), cout << endl; }
+
+
+
+
+
+void solve() {
+	ll i, j, n, m;
+	cin >> n;
+	ll x = n;
+	Yes(!(x && (!(x&(x-1)))));
+
+}
+
+
+
+
+
+
+
+
+
+int main() {
+
+    __speed() tt solve();
+
+    return 0;
+}
