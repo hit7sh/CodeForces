d6e2e44f76c759a82ea20be537fe8aca6bb3f97e
Hitesh Kumar Saini <hit7sh@gmail.com>
2021-03-30 13:10:47 +0530

    Create D.cpp

---
+++ Round 711 div 2/D.cpp
@@ -0,0 +1,33 @@
+ll ceil_div(ll a, ll b) {
+	return (a + b - 1) / b;
+}
+
+
+void solve() {
+	int N, M;
+    cin >> N >> M;
+    vi possible(M + 1, INF), ans(M + 1, INF);
+    possible[0] = ans[0] = 0;	
+ 
+    for (int i = 0; i < N; i++) {
+        int t, x, y, to;
+        cin >> t >> x >> y;
+ 
+        for (int j = 0; j <= M; j++) {
+        	if (t == 1) to = j + ceil_div(x, 1e5);
+        	else to = ceil_div(j * x, 1e5);
+
+            if (possible[j] + 1 <= y && to <= M)
+                possible[to] = min(possible[to], possible[j] + 1);
+    	}
+
+        for (int j = 1; j <= M; j++)
+            if (possible[j] < INF) {
+                ans[j] = min(ans[j], i + 1);
+                possible[j] = 0;
+            }
+    }
+ 
+    for (int j = 1; j <= M; j++)
+        cout << (ans[j] < INF ? ans[j] : -1) << ' ';
+}
